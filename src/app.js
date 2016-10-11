cbcc644a23b9522b90077e8288ca902eed44168f
Philip Alekseev <phil.alekseev@gmail.com>
2016-10-10 22:58:58 -0400

    Add Login state conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import Config from 'react-native-config'
 import LoginForm from './components/LoginForm';
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
     apiKey: Config.FIREBASE_API_KEY,
@@ -13,16 +15,45 @@ class App extends Component {
     storageBucket: `${Config.FIREBASE_HOST_URL}.appspot.com`,
     messagingSenderId: `${Config.FIREBASE_MESSAGE_ID}`
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+        case true:
+          return <Button>Log Out</Button>;
+        case false:
+          return <LoginForm />;
+        default:
+          return (
+            <View style={styles.spinnerStyle}>
+              <Spinner />
+            </View>
+          );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  spinnerStyle: {
+    marginVertical: 200
+  }
+};
 
 export default App;

171aed5a869c94e94f65c6da1963dec176a2d4f0
Philip Alekseev <phil.alekseev@gmail.com>
2016-10-10 22:23:39 -0400

    Add Basic Login with Firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' })
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication Failed.' });
+        });
+      });
+  }
 
   render() {
     return (
@@ -24,13 +40,24 @@ class LoginForm extends Component {
             secureTextEntry
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
